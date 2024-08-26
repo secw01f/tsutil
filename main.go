dfbc283669cc4431ececfd13eaab3061b7df03df
secw01f <16842677+secw01f@users.noreply.github.com>
2024-08-25 21:00:33 -0400

    added json format option for device details output

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -26,12 +28,23 @@ var (
 	deleteself    = delete.Flag("self", "Delete the this Tailscale endpoint").Bool()
 	deleteid      = delete.Flag("id", "Device ID to delete").String()
 	device        = app.Command("device", "Details about a device")
+	devicejson    = device.Flag("json", "Output device details in JSON").Bool()
 	detailself    = device.Flag("self", "Details about this device").Bool()
 	detailsid     = device.Flag("id", "Device ID").String()
+	devices       = app.Command("devices", "List all devices on the tailnet")
+	devicesjson   = devices.Flag("json", "Output list of all devices on the tialnet in JSON").Bool()
 	logout        = app.Command("logout", "Logout of Tailscale")
 )
 
 var authkey string
+
+type Device struct {
+	NodeID        string
+	Username      string
+	Hostname      string
+	OS            string
+	ClientVersion string
+}
 
 func main() {
 
@@ -104,12 +117,65 @@ func main() {
 			tscid := strings.Split(*tscs, "-")[2]
 			authkey = GetTsApi(tscid, *tscs)
 			details := TsDevice(authkey, TsDeviceId())
-			fmt.Println(*details)
+			if *devicejson {
+				devicedetails := Device{NodeID: details.NodeID, Username: details.User, Hostname: details.Hostname, OS: details.OS, ClientVersion: details.ClientVersion}
+				devicedata, err := json.MarshalIndent(devicedetails, "", "    ")
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(devicedata))
+			} else {
+				fmt.Println("DeviceID:", details.NodeID)
+				fmt.Println("Username:", details.User)
+				fmt.Println("Hostname:", details.Hostname)
+				fmt.Println("OS:", details.OS)
+				fmt.Println("Client Version:", details.ClientVersion)
+			}
 		} else {
 			tscid := strings.Split(*tscs, "-")[2]
 			authkey = GetTsApi(tscid, *tscs)
 			details := TsDevice(authkey, *detailsid)
-			fmt.Println(*details)
+			if *devicejson {
+				devicedetails := Device{NodeID: details.NodeID, Username: details.User, Hostname: details.Hostname, OS: details.OS, ClientVersion: details.ClientVersion}
+				devicedata, err := json.MarshalIndent(devicedetails, "", "    ")
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(devicedata))
+			} else {
+				fmt.Println("DeviceID:", details.NodeID)
+				fmt.Println("Username:", details.User)
+				fmt.Println("Hostname:", details.Hostname)
+				fmt.Println("OS:", details.OS)
+				fmt.Println("Client Version:", details.ClientVersion)
+			}
+		}
+	case devices.FullCommand():
+		type Devices struct {
+			Devices []Device `json:"devices"`
+		}
+		tscid := strings.Split(*tscs, "-")[2]
+		authkey = GetTsApi(tscid, *tscs)
+		devices := TsDevices(authkey)
+		if *devicesjson {
+			jsondevices := Devices{Devices: []Device{}}
+			for _, device := range devices {
+				devicedetails := Device{NodeID: device.NodeID, Username: device.User, Hostname: device.Hostname, OS: device.OS, ClientVersion: device.ClientVersion}
+				jsondevices.Devices = append(jsondevices.Devices, devicedetails)
+			}
+			devicesdata, err := json.MarshalIndent(jsondevices, "", "    ")
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(devicesdata))
+		} else {
+			for _, device := range devices {
+				fmt.Println("\nDeviceID:", device.NodeID)
+				fmt.Println("Username:", device.User)
+				fmt.Println("Hostname:", device.Hostname)
+				fmt.Println("OS:", device.OS)
+				fmt.Printf("Client Version: %s\n", device.ClientVersion)
+			}
 		}
 	case logout.FullCommand():
 		Logout()
